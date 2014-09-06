6898e61c719e95c65e84f3ae05649d868822840d
Samir Cury <samir@hep.caltech.edu>
2014-09-05 19:46:44 -0700

    First couchapp working version. Missing view grouping config

---
+++ couchapp/_attachments/script/input.js
@@ -0,0 +1,13 @@
+{"rows":[
+{"key":"AMD Opteron(tm) Processor 6378","value":{"sum":6740.937400000001,"count":221,"min":20.3378,"max":35.3746,"sumsqr":207388.65452142002}},
+{"key":"Intel(R) Xeon(R) CPU 5160 @ 3.00GHz","value":{"sum":128.65169999999998,"count":6,"min":20.8541,"max":22.441,"sumsqr":2760.09050945}},
+{"key":"Intel(R) Xeon(R) CPU E5-2650 0 @ 2.00GHz, Intel(R) Xeon(R) CPU E5-2660 0 @ 2.20GHz","value":{"sum":1210.2122,"count":32,"min":37.2614,"max":38.4101,"sumsqr":45771.56801974}},
+{"key":"Intel(R) Xeon(R) CPU E5-2660 0 @ 2.20GHz","value":{"sum":556.5585000000001,"count":16,"min":22.9456,"max":43.3078,"sumsqr":19789.64865463}},
+{"key":"Intel(R) Xeon(R) CPU E5-2670 0 @ 2.60GHz","value":{"sum":1762.6645999999998,"count":46,"min":19.061,"max":42.4281,"sumsqr":69017.23947546}},
+{"key":"Intel(R) Xeon(R) CPU E5345 @ 2.33GHz","value":{"sum":171.2951,"count":6,"min":26.0473,"max":39.7679,"sumsqr":5041.54506313}},
+{"key":"Intel(R) Xeon(R) CPU E5630 @ 2.53GHz","value":{"sum":470.9636,"count":11,"min":42.5247,"max":43.1473,"sumsqr":20164.68004824}},
+{"key":"Intel(R) Xeon(R) CPU L5420 @ 2.50GHz","value":{"sum":771.8971000000001,"count":25,"min":21.8562,"max":36.1163,"sumsqr":24714.197119829998}},
+{"key":"Intel(R) Xeon(R) CPU L5520 @ 2.27GHz","value":{"sum":356.4513,"count":16,"min":21.4959,"max":23.1206,"sumsqr":7948.8194948499995}},
+{"key":"Intel(R) Xeon(R) CPU L5630 @ 2.13GHz","value":{"sum":132.60160000000002,"count":3,"min":37.9799,"max":47.5142,"sumsqr":5919.1885619}},
+{"key":"Intel(R) Xeon(R) CPU L5640 @ 2.27GHz","value":{"sum":689.7071,"count":29,"min":22.049,"max":33.5067,"sumsqr":16565.14034463}}
+]}
