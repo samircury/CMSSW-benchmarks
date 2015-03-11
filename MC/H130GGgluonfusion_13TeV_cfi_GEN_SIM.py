f5fa0907656428b4028d48d95a84f672250d4122
samir <samir@positron03.hep.caltech.edu>
2015-03-11 14:03:56 -0700

    adding the right MC PSet

---
+++ MC/H130GGgluonfusion_13TeV_cfi_GEN_SIM.py
@@ -0,0 +1,190 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: H130GGgluonfusion_13TeV_cfi --conditions auto:run2_mc -n 10 --eventcontent FEVTDEBUG --relval 9000,100 -s GEN,SIM --datatier GEN-SIM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1 --magField 38T_PostLS1 --fileout file:step1.root
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('SIM')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.Geometry.GeometrySimDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+
+
+#process.FastTimerService = cms.Service("FastTimerService",
+#    dqmPath = cms.untracked.string('DQM/TimerService'),
+#    dqmModuleTimeRange = cms.untracked.double(40.0),
+#    luminosityProduct = cms.untracked.InputTag("scalersRawToDigi"),
+#    enableDQMbyLuminosity = cms.untracked.bool(True),
+#    enableTimingModules = cms.untracked.bool(False),
+#    enableDQMbyPathOverhead = cms.untracked.bool(False),
+#    enableDQMbyModule = cms.untracked.bool(False),
+#    dqmLuminosityResolution = cms.untracked.double(1e+31),
+#    enableTimingExclusive = cms.untracked.bool(False),
+#    skipFirstPath = cms.untracked.bool(False),
+#    enableDQMbyLumiSection = cms.untracked.bool(True),
+#    dqmPathTimeResolution = cms.untracked.double(10),
+#    dqmPathTimeRange = cms.untracked.double(10000),
+#    enableTimingSummary = cms.untracked.bool(False),
+#    dqmTimeRange = cms.untracked.double(10000),
+#    dqmLumiSectionsRange = cms.untracked.uint32(2500),
+#    enableDQMbyProcesses = cms.untracked.bool(False),
+#    enableDQMSummary = cms.untracked.bool(True),
+#    useRealTimeClock = cms.untracked.bool(False),
+#    enableDQMbyPathTotal = cms.untracked.bool(True),
+#    enableTimingPaths = cms.untracked.bool(False),
+#    enableDQMbyPathExclusive = cms.untracked.bool(False),
+#    dqmTimeResolution = cms.untracked.double(10),
+#    supportedProcesses = cms.untracked.vuint32(),
+#    dqmModuleTimeResolution = cms.untracked.double(0.2),
+#    dqmLuminosityRange = cms.untracked.double(1e+34),
+#    enableDQMbyPathActive = cms.untracked.bool(False),
+#    enableDQMbyPathDetails = cms.untracked.bool(True),
+#    enableDQM = cms.untracked.bool(True),
+#    enableDQMbyPathCounters = cms.untracked.bool(True)
+#)
+
+process.add_(cms.Service("CPU"))
+
+process.Timing = cms.Service("Timing",
+    summaryOnly = cms.untracked.bool(True)
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(3)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.19 $'),
+    annotation = cms.untracked.string('H130GGgluonfusion_13TeV_cfi nevts:10'),
+    name = cms.untracked.string('Applications')
+)
+
+# Output definition
+
+process.FEVTDEBUGoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.FEVTDEBUGEventContent.outputCommands,
+    fileName = cms.untracked.string('file:step1.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
+
+process.generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.0),
+    crossSection = cms.untracked.double(0.05),
+    maxEventsToPrint = cms.untracked.int32(3),
+    PythiaParameters = cms.PSet(
+        pythiaUESettings = cms.vstring('MSTU(21)=1     ! Check on possible errors during program execution', 
+            'MSTJ(22)=2     ! Decay those unstable particles', 
+            'PARJ(71)=10 .  ! for which ctau  10 mm', 
+            'MSTP(33)=0     ! no K factors in hard cross sections', 
+            'MSTP(2)=1      ! which order running alphaS', 
+            'MSTP(51)=10042 ! structure function chosen (external PDF CTEQ6L1)', 
+            'MSTP(52)=2     ! work with LHAPDF', 
+            'PARP(82)=1.921 ! pt cutoff for multiparton interactions', 
+            'PARP(89)=1800. ! sqrts for which PARP82 is set', 
+            'PARP(90)=0.227 ! Multiple interactions: rescaling power', 
+            'MSTP(95)=6     ! CR (color reconnection parameters)', 
+            'PARP(77)=1.016 ! CR', 
+            'PARP(78)=0.538 ! CR', 
+            'PARP(80)=0.1   ! Prob. colored parton from BBR', 
+            'PARP(83)=0.356 ! Multiple interactions: matter distribution parameter', 
+            'PARP(84)=0.651 ! Multiple interactions: matter distribution parameter', 
+            'PARP(62)=1.025 ! ISR cutoff', 
+            'MSTP(91)=1     ! Gaussian primordial kT', 
+            'PARP(93)=10.0  ! primordial kT-max', 
+            'MSTP(81)=21    ! multiple parton interactions 1 is Pythia default', 
+            'MSTP(82)=4     ! Defines the multi-parton model'),
+        processParameters = cms.vstring('PMAS(25,1)=130.0      !mass of Higgs', 
+            'MSEL=0                  ! user selection for process', 
+            'MSUB(102)=1             !ggH', 
+            'MSUB(123)=0             !ZZ fusion to H', 
+            'MSUB(124)=0             !WW fusion to H', 
+            'MSUB(24)=0              !ZH production', 
+            'MSUB(26)=0              !WH production', 
+            'MSUB(121)=0             !gg to ttH', 
+            'MSUB(122)=0             !qq to ttH', 
+            'MDME(210,1)=0           !Higgs decay into dd', 
+            'MDME(211,1)=0           !Higgs decay into uu', 
+            'MDME(212,1)=0           !Higgs decay into ss', 
+            'MDME(213,1)=0           !Higgs decay into cc', 
+            'MDME(214,1)=0           !Higgs decay into bb', 
+            'MDME(215,1)=0           !Higgs decay into tt', 
+            'MDME(216,1)=0           !Higgs decay into', 
+            'MDME(217,1)=0           !Higgs decay into Higgs decay', 
+            'MDME(218,1)=0           !Higgs decay into e nu e', 
+            'MDME(219,1)=0           !Higgs decay into mu nu mu', 
+            'MDME(220,1)=0           !Higgs decay into tau nu tau', 
+            'MDME(221,1)=0           !Higgs decay into Higgs decay', 
+            'MDME(222,1)=0           !Higgs decay into g g', 
+            'MDME(223,1)=1           !Higgs decay into gam gam', 
+            'MDME(224,1)=0           !Higgs decay into gam Z', 
+            'MDME(225,1)=0           !Higgs decay into Z Z', 
+            'MDME(226,1)=0           !Higgs decay into W W'),
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.FEVTDEBUGoutput_step = cms.EndPath(process.FEVTDEBUGoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.FEVTDEBUGoutput_step)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.generator * getattr(process,path)._seq 
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from SLHCUpgradeSimulations.Configuration.postLS1Customs
+from SLHCUpgradeSimulations.Configuration.postLS1Customs import customisePostLS1 
+
+#call to customisation function customisePostLS1 imported from SLHCUpgradeSimulations.Configuration.postLS1Customs
+process = customisePostLS1(process)
+
+# End of customisation functions
+
---
+++ MC/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+source /cvmfs/cms.cern.ch/cmsset_default.sh
