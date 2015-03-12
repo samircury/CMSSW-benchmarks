0024c815b0dc1cf6684722b818bf9be83d30f3f6
Samir Cury <samir@hep.caltech.edu>
2015-03-12 16:44:59 -0700

    sourcing run.sh works, running doesnt

--- MC/run.sh
+++ MC/run.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+mkdir scratch
+cd scratch
 source /cvmfs/cms.cern.ch/cmsset_default.sh
+cmsrel CMSSW_7_3_0
+cd CMSSW_7_3_0/src/
+cmsenv
+cmsRun -e ../../../H130GGgluonfusion_13TeV_cfi_GEN_SIM.py
+cd ../../..
