097ebb0b3808b12349c8adfa1bc667f307891fdd
Samir Cury <samircurys@gmail.com>
2015-03-13 11:02:21 -0700

    run.sh works by hand, condor still doesn't pick up

--- MC/run.sh
+++ MC/run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-mkdir scratch
-cd scratch
+mkdir /tmp/mcbenchmark
+cd /tmp/mcbenchmark
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-cmsrel CMSSW_7_3_0
+export SCRAM_ARCH=slc6_amd64_gcc491
+/cvmfs/cms.cern.ch/common/scramv1 project CMSSW CMSSW_7_3_0 &> /dev/null
 cd CMSSW_7_3_0/src/
-cmsenv
-cmsRun -e ../../../H130GGgluonfusion_13TeV_cfi_GEN_SIM.py
-cd ../../..
+eval `/cvmfs/cms.cern.ch/common/scramv1 runtime -sh`
+cmsRun -e /tmp/CMSSW-benchmarks-master/MC/H130GGgluonfusion_13TeV_cfi_GEN_SIM.py &> cmssw.log
+AVG_TPE=$(grep AvgEventCPU FrameworkJobReport.xml | awk -F'"' '{print $4}')
+rm -Rf /tmp/mcbenchmark
+echo "CMSTimePerEventMC = $AVG_TPE
+--"
